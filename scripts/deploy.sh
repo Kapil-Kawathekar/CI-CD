a872bb5714c970fbae7b06d52b3ac0702c4e5b53
Kapil Kawathekar <63143747+Kapil-Kawathekar@users.noreply.github.com>
2024-12-09 19:39:24 +0530

    Update deploy.sh
    image tag updation in yaml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,3 +27,27 @@ echo "Pushing Docker image to GCP Container Registry..."
 docker push "$IMAGE_NAME"
 
 echo "Docker image successfully pushed: $IMAGE_NAME"
+
+# Update deployment.yaml file
+DEPLOYMENT_FILE="k8/deploy.yaml"
+
+echo "Updating deployment.yaml with image tag: $IMAGE_TAG"
+sed -i "s|image: <IMAGE>|image: ${IMAGE_TAG}|" $DEPLOYMENT_FILE
+
+echo "Updated deployment.yaml file:"
+cat $DEPLOYMENT_FILE
+
+# Commit and push the updated file
+echo "Setting Git user..."
+git config --global user.name "ci-cd-bot"
+git config --global user.email "ci-cd-bot@mydomain.com"
+
+# Commit and push changes if any
+if git diff --exit-code $DEPLOYMENT_FILE; then
+  echo "No changes detected in deployment.yaml, skipping commit."
+else
+  git add $DEPLOYMENT_FILE
+  git commit -m "Update image tag for $ENVIRONMENT environment to ${IMAGE_TAG}"
+  git push origin "$BRANCH"
+  echo "Changes committed and pushed to branch $BRANCH"
+fi
